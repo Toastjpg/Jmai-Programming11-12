efe9f730d2fdd0a4a5f6ebb19097c7b69cd262f6
Toastjpg <52947117+Toastjpg@users.noreply.github.com>
2019-09-12 22:43:23 -0700

    Added while loop, scoring system, exit

--- RockPaperScissors/src/Main.java
+++ RockPaperScissors/src/Main.java
@@ -5,52 +5,88 @@ public class Main {
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
         //Scanner for user input
-        String userChoice = null;
+        String userChoice = "";
+        String userChoiceFull = "";
         //Stores user input as string
-        System.out.println("Choose one of the following: \"r\" for Rock \"p\" for Paper \"s\" for Scissors");
-        System.out.println("______________________________________________________________________________");
-        //Title and text
-        userChoice = userInput.nextLine().toLowerCase();
-        //Gets user input and makes lowercase
-        Random random = new Random();
-        //Random number generator
-        int randomNum = random.nextInt(3);
-        //Gets random int 0,1,or 2
-        String computerChoice;
-        //sets computer choice as string
+        int computerScore = 0;
+        int userScore = 0;
+        //sets both players score
 
-        if (randomNum == 0) {
-            computerChoice = "r";
-        } else if (randomNum == 1) {
-            computerChoice = "p";
-        } else {
-            computerChoice = "s";
-        }
-        //randomly generates computer choice
+        while ((userScore < 3) && (computerScore < 3) && (!userChoice.equals("e"))){
+            System.out.println("Choose one of the following: \"r\" for Rock \"p\" for Paper \"s\" for Scissors or \"e\" to exit the game.");
+            System.out.println("______________________________________________________________________________");
+            //Title and text
+            userChoice = userInput.nextLine().toLowerCase();
+            //Gets user input and makes lowercase
+            if (userChoice.equals("r")){
+                userChoiceFull = "rock";
+            }
+            else if (userChoice.equals("p")){
+                userChoiceFull = "paper";
+            }
+            else {
+                userChoiceFull = "scissors";
+            }
+            //Converts user input to full names
 
-        if (!userChoice.equals("r") && !userChoice.equals("p") && !userChoice.equals("s")){
-            System.out.println("Invalid input.");
+            Random random = new Random();
+            //Random number generator
+            int randomNum = random.nextInt(3);
+            //Gets random int 0,1,or 2
+            String computerChoice;
+            //sets computer choice as string
+            if (randomNum == 0) {
+                computerChoice = "r";
+            } else if (randomNum == 1) {
+                computerChoice = "p";
+            } else {
+                computerChoice = "s";
+            }
+            //randomly generates computer choice
+            if (!userChoice.equals("r") && !userChoice.equals("p") && !userChoice.equals("s") && !userChoice.equals("e")){
+                System.out.println("Invalid input.");
+            }
+            //Detects invalid inputs
+            if (userChoice.equals(computerChoice)) {
+                System.out.println("Tie! You both chose " + userChoiceFull);
+                System.out.println("Your Score: " + userScore + "\t Computer Score: " + computerScore + "\n");
+            } else if (userChoice.equals("r") && computerChoice.equals("s")) {
+                System.out.println("You win! Your opponent chose paper.");
+                userScore++;
+                System.out.println("Your Score: " + userScore + "\t Computer Score: " + computerScore + "\n");
+            } else if (userChoice.equals("p") && computerChoice.equals("r")) {
+                System.out.println("You win! Your opponent chose rock.");
+                userScore++;
+                System.out.println("Your Score: " + userScore + "\t Computer Score: " + computerScore + "\n");
+            } else if (userChoice.equals("s") && computerChoice.equals("p")) {
+                System.out.println("You win! Your opponent chose paper.");
+                userScore++;
+                System.out.println("Your Score: " + userScore + "\t Computer Score: " + computerScore + "\n");
+            } else if (userChoice.equals("r") && computerChoice.equals("p")) {
+                System.out.println("You lose! Your opponent chose paper.");
+                computerScore++;
+                System.out.println("Your Score: " + userScore + "\t Computer Score: " + computerScore + "\n");
+            } else if (userChoice.equals("p") && computerChoice.equals("s")) {
+                System.out.println("You lose! Your opponent chose scissors.");
+                computerScore++;
+                System.out.println("Your Score: " + userScore + "\t Computer Score: " + computerScore + "\n");
+            } else if (userChoice.equals("s") && computerChoice.equals("r")) {
+                System.out.println("You lose! Your opponent chose rock.");
+                computerScore++;
+                System.out.println("Your Score: " + userScore + "\t Computer Score: " + computerScore + "\n");
+            }
         }
-        if (userChoice.equals(computerChoice)){
-            System.out.println("Tie! You both chose " + userChoice);
+        if (userChoice.equals("e")){
+            System.out.println("Thank you for playing! Goodbye.");
         }
-        else if (userChoice.equals("r") && computerChoice.equals("s")){
-            System.out.println("You win! Your opponent chose paper.");
+        else if (userScore > computerScore){
+            System.out.println("Victory! You have beat the computer!");
+            System.out.println("Your Score: " + userScore + "\t Computer Score: " + computerScore + "\n");
         }
-        else if (userChoice.equals("p") && computerChoice.equals("r")){
-            System.out.println("You win! Your opponent chose rock.");
+        else {
+            System.out.println("Defeat! You have been beaten by the computer!");
+            System.out.println("Your Score: " + userScore + "\t Computer Score: " + computerScore + "\n");
         }
-        else if (userChoice.equals("s") && computerChoice.equals("p")){
-            System.out.println("You win! Your opponent chose paper.");
-        }
-        else if (userChoice.equals("r") && computerChoice.equals("p")){
-            System.out.println("You lose! Your opponent chose paper.");
-        }
-        else if (userChoice.equals("p") && computerChoice.equals("s")){
-            System.out.println("You lose! Your opponent chose scissors.");
-        }
-        else if (userChoice.equals("s") && computerChoice.equals("r")){
-            System.out.println("You lose! Your opponent chose rock.");
-        }
+        //Detects if player won or lost the game. Also detects if they exited
     }
 }
