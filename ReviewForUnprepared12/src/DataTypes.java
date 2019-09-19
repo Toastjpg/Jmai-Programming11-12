15fb0e5e826f806cec81bb233e43c80bcf37bffc
Toastjpg <52947117+Toastjpg@users.noreply.github.com>
2019-09-19 11:35:34 -0700

    new review stuff

---
+++ ReviewForUnprepared12/src/DataTypes.java
@@ -0,0 +1,12 @@
+public class DataTypes {
+    private boolean stayInData = true;
+
+    public DataTypes{
+        dataTypesStart();
+    }
+    private void dataTypesStart{
+        while (stayInData){
+
+        }
+    }
+}
