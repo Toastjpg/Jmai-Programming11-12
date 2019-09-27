a1f2fe3eebf57f49c56410ff78f7cb4abf6ffa16
Toastjpg <52947117+Toastjpg@users.noreply.github.com>
2019-09-27 11:29:01 -0700

    halfway done controlflow messages

--- ReviewForUnprepared12/src/ControlFlow.java
+++ ReviewForUnprepared12/src/ControlFlow.java
@@ -8,12 +8,16 @@ public class ControlFlow {
     private final String GREETMESSAGE = "Control Flow Options:\n1. If/Else If/Else Statements\n2. While Loop/For Loops\n3. Switch Statements\n0. Exit to Main Menu";
     //Info and examples
     private final String IFSTATEMENTS = "If statements are used in java to let the system make decisions.  It creates branching programs.  If statements are based on booleans.  If the condition is true then the block of code will execute.  If the statement is false the block of code will be skipped over and the program continues to run.";
-    private final String IFSTATEMENTSEXAMPLE = "Example:\nif(condition){\n\tblock of code\n}";
-    private final String FORLOOPS = "FORLOOPSTATEMENTS";
-    private final String FORLOOPSEXAMPLE = "FORLOOPEXAMPLE";
-    private final String WHILELOOPS = "WHILELOOPS";
-    private final String WHILELOOPSEXAMPLE = "WHILELOOPEXAMPLE";
-    private final String SWITCHSTATEMENT = "SWITCHSTATEMENT";
+    private final String IFSTATEMENTSEXAMPLE = "Example:\nif(condition){\n\tblock of code\n}\n";
+    private final String ELSEIFSTATEMENTS = "Else If statements will run if the initial \"if statement\" is false and its own conditions are met. Multiple Else If statements can run in order until a condition is met or it reaches the end of the branch with no conditions met.";
+    private final String ELSEIFSTATEMENTSEXAMPLE = "Example:\nif(condition){\n\tblock of code\n}\nelse if(condition){\n\tblock of code\n";
+    private final String ELSESTATEMENTS = "Else statements run when the preceding If statements and Else If statements are all false. This will run regardless and does not require a condition.";
+    private final String ELSESTATEMENTSEXAMPLE = "Example:\nelse{\n\tblock of code\n}\n";
+    private final String WHILELOOPS = "While Loops are a used to continuously execute statements as long as initial condition are still true. The block of code within the loop will continue to run until the condition is false and the loop breaks.";
+    private final String WHILELOOPSEXAMPLE = "Example:\nwhile(condition){\n\tstatement(s)\n}\n";
+    private final String FORLOOPS = "For Loops are another form of loops. They are similar to While Loops but have three conditions. The first condition initializes the variable, the second condition sets the amount of iterations by limiting the variable value, and the third condition changes the increments at which the variable changes at. For Loops are useful because they have as set amount of iterations whereas While Loops can run forever.";
+    private final String FORLOOPSEXAMPLE = "Example:\nfor(int i = 0; i <= 10; i++){\nstatement(s)\n}\nThe statements will run until int i = 10 with each iteration increasing i by 1\n";
+    private final String SWITCHSTATEMENT = "";
     private final String SWITCHSTATEMENTEXAMPLE = "SWITCH EXAMPLE";
 
     public ControlFlow() {
@@ -43,12 +47,17 @@ public class ControlFlow {
     private void printIfMessages(){
         System.out.println(IFSTATEMENTS);
         System.out.println(IFSTATEMENTSEXAMPLE);
+        System.out.println(ELSEIFSTATEMENTS);
+        System.out.println(ELSEIFSTATEMENTSEXAMPLE);
+        System.out.println(ELSESTATEMENTS);
+        System.out.println(ELSESTATEMENTSEXAMPLE);
     }
     private void printLoopMessages(){
-        System.out.println(FORLOOPS);
-        System.out.println(FORLOOPSEXAMPLE);
         System.out.println(WHILELOOPS);
         System.out.println(WHILELOOPSEXAMPLE);
+        System.out.println(FORLOOPS);
+        System.out.println(FORLOOPSEXAMPLE);
+
     }
     private void printSwitchMessages(){
         System.out.println(SWITCHSTATEMENT);
--- ReviewForUnprepared12/src/Objects.java
+++ ReviewForUnprepared12/src/Objects.java
@@ -4,6 +4,16 @@ public class Objects {
     private boolean stayInObjects = true;
     private int userChoice;
     Scanner scanner = new Scanner(System.in);
+    private final String GREETMESSAGE = "Objects Options:\n1. How to Create a Class\n2. What is a Constructor\n3. Setters/Getters\n4. toString Method\n 5. How to Access Data Inside an Object\n0. Exit to Main Menu";
+    private final String CLASS = "";
+    private final String CLASSEXAMPLE = "";
+    private final String CONSTRUCTOR = "";
+    private final String CONSTRUCTOREXAMPLE = "";
+    private final String SETTERSGETTERS = "";
+    private final String SETTERSGETTERSEXAMPLE = "";
+    private final String TOSTRINGMETHOD = "";
+    private final String TOSTRINGMETHODEXAMPLE = "";
+    private final String ACCESSDATA = "";
 
     private void objectsStart(){
         while (stayInObjects){
