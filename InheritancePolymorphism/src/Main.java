609cc1be5f14c49c5d775e93d8b9d572ce29dfa1
Toastjpg <52947117+Toastjpg@users.noreply.github.com>
2019-10-24 11:27:49 -0700

    frick

--- InheritancePolymorphism/src/Main.java
+++ InheritancePolymorphism/src/Main.java
@@ -1,14 +1,24 @@
 class A{
-    int i = 10;
-}
+    public A(int i){
+        System.out.println(1);
+    }
 
-class B extends A{
-    int i = 20;
+    public A(){
+        this(10);
+        System.out.println(2);
+    }
+    void A(){
+        A(10);
+        System.out.println(3);
+    }
+
+    void A(int i){
+
+    }
 }
 
 public class Main{
     public static void main(String[] args) {
-        A a = new B();
-        System.out.println(a.i);
+        new A().A();
     }
 }
