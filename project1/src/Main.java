3a0f4b975bb021b24059cc6c2a7126bcf617a13f
Toastjpg <52947117+Toastjpg@users.noreply.github.com>
2019-11-01 01:04:55 -0700

    started new proj

---
+++ project1/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        StartMenu startMenu = new StartMenu();
+        while (startMenu.stay){
+        }
+    }
+}
---
+++ project1/src/StartMenu.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class StartMenu {
+    public static boolean stay = true;
+    Scanner scanner = new Scanner(System.in);
+    int userChoice;
+
+    public void introMessage(){
+        System.out.println("asjkgnasjgkd");
+    }
+
+    public StartMenu(){
+        introMessage();
+    }
+
+
+}
