b3905ca6cc23f384a1e59ed9ea1be49ea3b66764
Toastjpg <52947117+Toastjpg@users.noreply.github.com>
2019-11-06 18:26:36 -0800

    -a

---
+++ project1/src/Class.java
@@ -0,0 +1,15 @@
+public class Class {
+    int maxHp = 100;
+    int hp = 100;
+    int maxMana = 100;
+    int mana = 100;
+    int atkDmg = 20;
+
+    public Class(int maxHp, int hp, int maxMana, int mana, int atkDmg) {
+        this.maxHp = maxHp;
+        this.hp = hp;
+        this.maxMana = maxMana;
+        this.mana = mana;
+        this.atkDmg = atkDmg;
+    }
+}
---
+++ project1/src/ClassWarrior.java
@@ -0,0 +1,6 @@
+public class ClassWarrior extends Class{
+    ClassWarrior(int maxHp, int hp, int maxMana, int mana, int atkDmg){
+        super(maxHp, hp, maxMana, mana, atkDmg);
+
+    }
+}
