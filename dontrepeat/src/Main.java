f68e7e8747129a5becf2a926e60722658c95757a
Toastjpg <52947117+Toastjpg@users.noreply.github.com>
2020-01-28 21:43:46 -0800

    finished dontrepeat

--- dontrepeat/src/Main.java
+++ dontrepeat/src/Main.java
@@ -1,11 +1,19 @@
-import java.util.ArrayList;
-
 public class Main {
-    public static int dayOfYear(int month, int dayOfMonth, int year) {
-
+    public static String dayOfYear(int month, int dayOfMonth, int year) {
+        int dayOfYear = 0;
+        int[] totalDayInMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        if (month >= 1 && month <= 12 && dayOfMonth > 0 && dayOfMonth <= totalDayInMonth[month - 1] && year > 0){
+            for(int i = 1; i <= month - 1; i++){
+                dayOfYear += totalDayInMonth[i];
+            }
+            dayOfYear += dayOfMonth;
+        }
+        else{
+            return "invalid input(s)";
+        }
+        return String.valueOf(dayOfYear);
     }
-
     public static void main(String[] args) {
-        System.out.println(dayOfYear(1,17,2020));
+        System.out.println(dayOfYear(1, 29,2062));
     }
 }
