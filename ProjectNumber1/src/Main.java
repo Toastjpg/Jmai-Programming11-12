350d6e7e5f8efd5b0696982b7b18acc6d71a41a5
Toastjpg <52947117+Toastjpg@users.noreply.github.com>
2020-02-28 15:01:42 -0800

    fkcjk

---
+++ ProjectNumber1/src/Main.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    static int problemOne(String s){
+        int answer = 0;
+        ArrayList characters = new ArrayList();
+        for (int i = 0; i < s.length(); i++){
+            char x = s.charAt(i);
+            characters.add(x);
+            if ( x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u'){
+                answer++;
+            }
+        }
+        return answer;
+    }
+
+    static int problemTwo(String s){
+        int answer = 0;
+        ArrayList characters = new ArrayList();
+        if (s.contains("bob")){
+            answer++;
+        }
+        return answer;
+    }
+
+    static String problemThree(String s){
+        return s;
+    }
+
+    public static void main(String[] args) {
+        /*
+        Set s to a string and run your method using s as the parameter
+        Run your method in a println statement to determine what the output was
+        Once you think you have it working try running the tests.
+        The tests will put your method through several different Strings to test
+        all possible cases.  If you have 100% success then there is no bugs in your methods.
+         */
+        String s = "azcbobobegghakl";
+        System.out.println("Problem #1: " + problemOne(s));
+        System.out.println("Problem #2: " + problemTwo(s));
+    }
+}
