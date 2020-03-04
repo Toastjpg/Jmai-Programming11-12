f2d16c2b0b3eff9cae13425dd66380cd198fd313
Toastjpg <52947117+Toastjpg@users.noreply.github.com>
2020-03-03 22:54:35 -0800

    done

--- CreateTestDocumentation/Tests/Test.java
+++ CreateTestDocumentation/Tests/Test.java
@@ -1,4 +1,17 @@
 public class Test {
-    /*
-    * */
+    /**
+     * Testing Strategy
+     *
+     * Test when listOfLists.size() is >2, <2, and =2
+     * Test when lists are combined, discarded or mutated
+     * Test that all lists contain data type T
+     */
+
+    /**
+     * Test combineFirstTwo()
+     *
+     * Covers when listOfLists is >2, <2 and =2
+     * if list is >2 or =2, the second list is removed
+     * if list is <2, nothing happens
+     */
 }
